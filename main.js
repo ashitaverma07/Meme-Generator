9e2d5061b88fbc84fd76eac8f220c32d493ef6c0
ashitaverma07 <ashitaverma14@gmail.com>
2024-03-07 22:15:34 +0530

    5th

--- main.js
+++ main.js
@@ -6,13 +6,15 @@ const memeAuthor = document.querySelector(".meme-generator .meme-author");
 
 const updateDetails = (url, title, author) =>{
     memeImage.setAttribute("src", url);
-}
+    memeTitle.innerHTML = title;
+    memeAuthor.innerHTML = author;
+};
 
 const generateMeme = () => {
     fetch("https://meme-api.com/gimme/wholesomememes")
     .then(response => response.json())
     .then(data => {
         updateDetails(data.url, data.title, data.author);
-    })
+    });
 };
 generateMemeBtn.addEventListener("click", generateMeme);
