d371020b370b926be84629150ad17692fa55b537
ashitaverma07 <ashitaverma14@gmail.com>
2024-03-07 22:07:32 +0530

    specify subbredit

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-const 
+const generateMemeBtn = document.querySelector(".meme-generator .generate-meme-btn");
+
+const memeImage = document.querySelector(".meme-generator img");
+const memeTitle = document.querySelector(".meme-generator .meme-title");
+const memeAuthor = document.querySelector(".meme-generator .meme-author");
+
+
+const generateMeme = () => {
+    fetch("https://meme-api.com/gimme/wholesomememes").then(response => response.json().then(data => {
+
+    })
+    );
+};
+generateMemeBtn.addEventListener("click", generateMeme);
