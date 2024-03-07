4808766a2218da66467b8f0d51107ce6aaf403f3
ashitaverma07 <ashitaverma14@gmail.com>
2024-03-07 22:13:29 +0530

    urls connection

--- main.js
+++ main.js
@@ -4,11 +4,15 @@ const memeImage = document.querySelector(".meme-generator img");
 const memeTitle = document.querySelector(".meme-generator .meme-title");
 const memeAuthor = document.querySelector(".meme-generator .meme-author");
 
+const updateDetails = () =>{
+    memeImage.setAttribute("src", data.url);
+}
 
 const generateMeme = () => {
-    fetch("https://meme-api.com/gimme/wholesomememes").then(response => response.json().then(data => {
-
+    fetch("https://meme-api.com/gimme/wholesomememes")
+    .then(response => response.json())
+    .then(data => {
+        updateDetails(data.url, data.title, data.author);
     })
-    );
 };
 generateMemeBtn.addEventListener("click", generateMeme);
